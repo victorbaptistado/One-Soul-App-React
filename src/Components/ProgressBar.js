0312516983711043c6287cba3162f95c071c96af
victorbaptistado <victorbaptistado@gmail.com>
2021-11-28 21:58:54 +0000

    try on Progress bar

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,28 +1,65 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useRef } from 'react';
+
+
 
 const ProgressBar = ({text}) => {
 
-
-
     const [ progress, setProgress] = useState("");
 
+  
+  
+  
+  
+  
 
-    useEffect(() => {
-        setProgress(
-            function calculation (){
-                const elements = []; 
-            for(let i = 0; i < text.id; i++)
-            {
-                let corno = elements.push(<div>hi</div>)
-                return corno++;
-                
-            }}
-     ) }, [])
- 
+    useEffect(()=>{
+           setProgress(
+      function widthUpdate (){
+        let width = "0rem";
+       
+     
+            switch(text.id){
+            case 3:
+            let jonas = {width: "2rem"};      
+    
+            break;   
+         
+            case 4: 
+            width = "6rem";
+     
+            break;
 
 
+            }
+            return <div className="internalProgress" style={{}}>{widthUpdate}</div>;
+        }       )
+    }, [])
+
+
+
+           
+            
+        
+           /*
+            let you = getComputedStyle(trans).setProperty.width({widthnum});
+      */
+   
+            /*
+            let tro = document.getElementsByClassName("internalProgress");
+            let vamo = () => tro[0].setProperty("width", () =>calculation());
+            */
+       
+      
+      
+               
+             /*   let amplo = ref.current.style = "width: 3rem"; */
+                /*    let trans = document.querySelector(".internalProgress")*/
+        
+           
+     
     /*
     function hi(){
     for (const i = 0; i <= text.id; i++){
