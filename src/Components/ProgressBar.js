455d056f2ce575b9ac3033081ac112603eed03fa
victorbaptistado <victorbaptistado@gmail.com>
2021-12-02 09:47:42 +0000

    Progress bar Opacity upgrade

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -7,9 +7,9 @@ import { useRef } from 'react';
 
 const ProgressBar = ({text}) => {
 
-    const [ progress, setProgress] = useState("");
+    const [ progress, setProgress] = useState(0);
+    const [opacity, setOpacity] = useState(0);
 
-  
   
 
     useEffect(()=>{
@@ -18,12 +18,9 @@ const ProgressBar = ({text}) => {
       function widthUpdate(){        
             switch(text.id){
             case 3:
-            return 30;      
-            
-         
+            return 0;           
             case 4: 
-           return 40;
-     
+           return 30;
             case 5:
             return 50; 
             case 6:
@@ -42,10 +39,23 @@ const ProgressBar = ({text}) => {
          
 
 
+    useEffect(()=>{
+    setOpacity(
+      function opacityUpdate(){  
+            
+        if(text.id >= 4){
+        return 1;           
+ 
+        }}
+    )
+    
+    },[])
+
+
     return (
   <>
     <div className="externalProgress" >
-    <div className="internalProgress" style={{width: `${progress}%`}}>{progress}</div>
+    <div className="internalProgress" style={{width: `${progress}%`, opacity: `${opacity}`}}>{progress}</div>
     </div> 
         </>
     )
